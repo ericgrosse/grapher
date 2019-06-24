022e54a3cac07793484a10fda4b73bc456dc3e7c
Eric Grosse <ericgrosse1@gmail.com>
2019-06-24 13:06:36 -0400

    Setup Table component (for data entry)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.scss'
 
-function App() {
+function Table(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Table">
+      {
+        props.data.map((column, index) => (
+          <Column
+            key={index}
+            column={column}
+            index={index}
+            onChange={props.onChange}
+          />
+        ))
+      }
     </div>
-  );
+  )
 }
 
-export default App;
+function Row(props) {
+  return (
+    <div className="Row">
+      <input
+        key={`row${props.row}:column${props.column}`}
+        type="text"
+        value={props.cell}
+        onChange={props.onChange.bind(this, props.row, props.column)}
+      />
+    </div>
+  )
+}
+
+function Column(props) {
+  return (
+    <div className="Column">
+      {
+        props.column.map((row, index) => (
+          <Row
+            key={index}
+            cell={row}
+            row={index}
+            column={props.index}
+            onChange={props.onChange}
+          />
+        ))
+      }
+    </div>
+  )
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      table: [['x', 1, 2, 3], ['sin(x)', 0.84, 0.91, 0.14]]
+    }
+  }
+
+  handleChangeCell = (row, column, evt) => {
+    const tableDeepCopy = this.state.table.slice().map(x => x.slice())
+    tableDeepCopy[column][row] = evt.target.value
+    this.setState({ table: tableDeepCopy })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Grapher</h1>
+        
+        <Table
+          data={this.state.table}
+          onChange={this.handleChangeCell}
+        />
+      </div>
+    )
+  }
+}
+
+export default App
